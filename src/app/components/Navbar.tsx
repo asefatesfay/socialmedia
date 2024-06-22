bd30aa3bd056ee3bc971fd60fb03a4844b335a01
Assefa Tesfay <assefa.tesfay@nordstrom.com>
2024-06-22 07:01:35 -0700

    social media initial setup

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      {/* LEFT */}
+      <div className="">
+        <Link href="/">SocialMedia</Link>
+      </div>
+      {/* CENTER */}
+      <div className="">CENTER</div>
+      {/* RIGHT */}
+      <div className="">CENTER</div>
+    </div>
+  );
+};
+
+export default Navbar;
